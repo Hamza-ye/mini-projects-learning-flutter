bce9cd73102e1f22fbbfb60d83f01c5a449770da
Hamza-ye <7amza.it@gmail.com>
2020-06-03 18:19:05 +0300

    Create state_and_stateful_widget app and edit readme

--- beginner/state_and_stateful_widget/lib/main.dart
+++ beginner/state_and_stateful_widget/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:state_and_stateful_widget/widgets/car_widget.dart';
+
+import 'models/car.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,29 +16,42 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState("Cars");
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String title;
+  List<Car> _cars;
+
+  _MyHomePageState(this.title) {
+    _cars = [
+      Car("Bmw", "M3",
+          "Https://media.ed.edmunds-media.com/bmw/m3/2018/oem/2018_bmw_m3_sedan_base_fq_oem_4_150.jpg"),
+      Car("Nissan", "GTR",
+          "Https://media.ed.edmunds-media.com/nissan/gt-r/2018/oem/2018_nissan_gt-r_coupe_nismo_fq_oem_1_150.jpg"),
+      Car("Nissan", "Sentra",
+          "Https://media.ed.edmunds-media.com/nissan/sentra/2017/oem/2017_nissan_sentra_sedan_sr-turbo_fq_oem_4_150.jpg")
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<CarWidget> carWidgets = _cars.map((Car car) => CarWidget(car)).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(),
+      body: ListView(
+        children: carWidgets,
+      ),
     );
   }
 }
--- beginner/state_and_stateful_widget/lib/models/car.dart
+++ beginner/state_and_stateful_widget/lib/models/car.dart
@@ -7,6 +7,11 @@ class Car {
 
   operator ==(other) =>
       (other is Car) && (_make == other._make) && (_model == other._model);
-      
+
   int get hashCode => _make.hashCode ^ _model.hashCode ^ _imageSrc.hashCode;
+
+  get make => this._make;
+  get model => this._model;
+  get imageSrc => this._imageSrc;
+  
 }
