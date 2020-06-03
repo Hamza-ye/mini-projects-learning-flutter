8990a40d63c0d94064dfc3593c2c756089406f09
Hamza-ye <7amza.it@gmail.com>
2020-06-03 17:42:21 +0300

    Add state_and_stateful_widget App

---
+++ beginner/state_and_stateful_widget/android/app/src/main/kotlin/com/example/state_and_stateful_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_and_stateful_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ beginner/state_and_stateful_widget/lib/models/car.dart
@@ -0,0 +1,12 @@
+class Car {
+  String _make;
+  String _model;
+  String _imageSrc;
+
+  Car(this._make, this._model, this._imageSrc);
+
+  operator ==(other) =>
+      (other is Car) && (_make == other._make) && (_model == other._model);
+      
+  int get hashCode => _make.hashCode ^ _model.hashCode ^ _imageSrc.hashCode;
+}
